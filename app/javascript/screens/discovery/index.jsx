8afd40efdf87c48b29c78c3e2e24410c9dc8c494
João Victor <jovictor.miguel@gmail.com>
2020-04-15 18:24:19 -0300

    criado página discovery e configurado axios

--- app/javascript/screens/discovery/index.jsx
+++ app/javascript/screens/discovery/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import SectionWrapper from '../../components/common/section_wrapper'
+import Discovery from '../../components/discovery'
 import NavbarFooter from '../../components/common/navbar_footer'
 
 const DiscoveryScreen = () => {
   return (
     <>
-      <div>Discovery Screen</div>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
       <NavbarFooter />
     </>
   )
